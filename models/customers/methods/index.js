31df559ab2d9dc4baccce88979b2f9d4dce8af6f
Eric Mangpang <airrakedev@gmail.com>
2020-09-27 22:35:19 +0800

    Update user and admin authentication

--- models/customers/methods/index.js
+++ models/customers/methods/index.js
@@ -7,35 +7,35 @@ const saltRounds = 10;
 const methods = {}
 
 methods.setUserAuthentication = function (schema) {
-    let user = schema
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_PRIVATE_KEY);
-    return token
+   let user = schema
+   const token = jwt.sign({ _id: user._id, type: 'second' }, process.env.TOKEN_PRIVATE_KEY);
+   return token
 }
 
 
 methods.hashPassword = function (next, schema) {
-    let user = schema
+   let user = schema
 
-    if (!user.isModified('password')) return next()
+   if (!user.isModified('password')) return next()
 
 
-    bcrypt.genSalt(saltRounds, function (err, salt) {
-        if (err) return next(err)
+   bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err)
 
-        bcrypt.hash(user.password, salt, function (err, hash) {
-            if (err) return next(err)
+      bcrypt.hash(user.password, salt, function (err, hash) {
+         if (err) return next(err)
 
-            user.password = hash
-            next()
-        })
-    })
+         user.password = hash
+         next()
+      })
+   })
 }
 
 methods.comparePassword = async function (candidatePassword, schema) {
-    let user = schema
-    // return candidatePassword
-    let decrypt = await bcrypt.compare(candidatePassword, user.password);
-    return decrypt
+   let user = schema
+   // return candidatePassword
+   let decrypt = await bcrypt.compare(candidatePassword, user.password);
+   return decrypt
 }
 
 
