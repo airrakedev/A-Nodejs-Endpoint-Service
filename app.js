977ca3133f129b15264a615e2af569316f655204
Eric Mangpang <airrakedev@gmail.com>
2020-09-28 15:50:59 +0800

    Another fixed on PORT

--- app.js
+++ app.js
@@ -54,4 +54,4 @@ process.on('unhandledRejection', (error) => {
 // LOGGER
 morgan('tiny')
 
-app.listen(port || 5000, () => console.log(`Live at PORT ${port}`))
+app.listen(process.env.PORT || 4000, () => console.log(`Live at PORT ${process.env.PORT}`))
